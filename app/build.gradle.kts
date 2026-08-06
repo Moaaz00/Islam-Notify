9e3e3d03e921c162dcf27e309f8c18007d0bbb7f
Moaaz Mohamed <moaazamin285@gmail.com>
2026-08-06 23:26:21 +0300

    version code 3 (2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.islamnotify"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
